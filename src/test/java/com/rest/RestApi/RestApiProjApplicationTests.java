9669882f8946cd78f2a76ec667fbd382d1135d91
Eugine L <Eugine L@192.168.0.44>
2022-11-01 04:03:10 +0530

    Rest Proj commit

---
+++ src/test/java/com/rest/RestApi/RestApiProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.RestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
