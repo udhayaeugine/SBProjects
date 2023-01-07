306f85295b1396b24da2025d9a09cb7553d2afc9
Eugine L <Eugine L@EugisDell.home>
2023-01-07 14:30:33 +0530

    2nd test commit

--- src/main/java/com/rest/RestApi/RestApiProjApplication.java
+++ src/main/java/com/rest/RestApi/RestApiProjApplication.java
@@ -9,6 +9,7 @@ public class RestApiProjApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiProjApplication.class, args);
 		System.out.println("Welcome");
+		System.out.println("Test1");
 	}
 
 }
